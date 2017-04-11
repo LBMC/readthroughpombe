ae2401f9ab7baf9d93a4ae9839de86594c4e78ca
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-04-11 16:24:05 +0200

    file_handle.py: load necessary library
    Former-commit-id: e192ffd6954df9d35110e3d32c4a5cc4bdbd1bfb
    Former-commit-id: 9c14e8b96bd70f164036a04140591bf2d7c21cf2

---
+++ src/func/file_handle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+import sys
+import os.path
+import argparse
+import datetime
+import re
+import glob
+if sys.version_info[0] == 2:
+    print("file_handle.py is only compatible with python3.\
+    Please run file_handle.py as an executable or with the command\
+    'python3 file_handle.py'")
+    exit(1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="file_handle.py",
+        description="script to handle date in file name in the format \
+        'yyyy_mm_dd_filemames'. By default return the last file corresponding \
+        to the filename if existing or create it with the current date.")
+    parser.add_argument(
+        "-f",
+        help="input filename",
+        default=None,
+        action="store",
+        dest="input_file",
+        required=True,
+        nargs=1)
