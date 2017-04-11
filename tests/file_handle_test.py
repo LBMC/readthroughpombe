81b7ae837042e12881d4e0db44713f01d8afe21b
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-04-11 16:23:13 +0200

    file_handle_test.py: load_required files

---
+++ tests/file_handle_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+
+import unittest
+import datetime
+import os
+import sys
+sys.path.append(os.path.abspath("src/func/"))
+from file_handle import Dated_file
+
+
+
+if __name__ == '__main__':
+    unittest.main()
