c3df54d4c54e11aeae5f5cf61b60238ff7da3f33
lmodolo <laurent.modolo@ens-lyon.fr>
2017-05-11 09:43:52 +0200

    quality_control_test.sh: test for trimming with UrQt and Cutadapt

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq'
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'UrQt' && \
+ls -lR results/quality_control/ && \
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'cutadapt' && \
+ls -lR results/quality_control/
