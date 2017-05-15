be471f0cf601cbc07497b160bf7e922a602da3ee
lmodolo <laurent.modolo@ens-lyon.fr>
2017-05-15 17:15:03 +0200

    quality_control_test.sh: UrQt and trimmomatic tests
    Former-commit-id: fdfde837f4c5691035d79d7b7038cd01d13aad81
    Former-commit-id: 7a4983116becc6f51b60dbe0488ccab529855a76

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'UrQt' && \
-ls -lR results/quality_control/ && \
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'cutadapt' && \
-ls -lR results/quality_control/
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'UrQt'
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'cutadapt'
