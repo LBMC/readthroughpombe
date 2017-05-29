248d2e15f5947f0ff3ea24736c1add7e9d468b04
lmodolo <laurent.modolo@ens-lyon.fr>
2017-05-29 09:47:10 +0200

    init.sh: Kallisto installation procedure
    Former-commit-id: 8358d5bea899d6b4d2f24f7b4e4369698944047e
    Former-commit-id: 112f6dc106d7046c270086be7e54444a7304e1de

--- src/init.sh
+++ src/init.sh
@@ -15,6 +15,16 @@ git clone https://github.com/COMBINE-lab/salmon.git
 cd /tmp/salmon
 mkdir build
 cd build
-cmake -DFETCH_BOOST=TRUE -DCMAKE_INSTALL_PREFIX=/usr/bin/
+cmake -DFETCH_BOOST=TRUE -DCMAKE_INSTALL_PREFIX=/usr/
+make
+sudo make install
+rm -Rf /tmp/salmon
+
+cd /tmp
+git clone https://github.com/pachterlab/kallisto.git
+cd /tmp/kallisto
+mkdir build
+cd build
+cmake ..
 make
 sudo make install
