f98a6c44cc9e2048a2c0836f0d7ea36366ad7615
lmodolo <laurent.modolo@ens-lyon.fr>
2017-05-29 09:37:17 +0200

    init.sh: install salmon
    Former-commit-id: e29e2cfede2f6ed65e341bdb7031ace6b2244c6e
    Former-commit-id: d1ba9387925d3bec1a1b1f451a6dcfb8397be9ab

--- src/init.sh
+++ src/init.sh
@@ -9,3 +9,12 @@ pip2 install multiqc
 pip2 install click
 pip2 install jinja2
 sudo pip install cutadapt
+
+cd /tmp
+git clone https://github.com/COMBINE-lab/salmon.git
+cd /tmp/salmon
+mkdir build
+cd build
+cmake -DFETCH_BOOST=TRUE -DCMAKE_INSTALL_PREFIX=/usr/bin/
+make
+sudo make install
