bb0917f6cab3c6657a8e12e9e6aca05ada9c385b
lmodolo <laurent.modolo@ens-lyon.fr>
2017-06-13 15:42:01 +0200

    quality_control_test.sh: fix test with docker to build img
    Former-commit-id: 4196d9dd079fd608fe8caf892265a774f15d44d0
    Former-commit-id: 91119ab44ace7f8f51ef90bfef4d03fa05bb17f8

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -2,4 +2,5 @@
 # bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'UrQt' --paired false -resume && \
 # bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*.fastq' --trimmer 'cutadapt' --paired false -resume && \
 # bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*_R{1,2}.fastq.gz' --trimmer 'UrQt' --paired true -resume
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*_R{1,2}.fastq.gz' --trimmer 'UrQt' --paired true -resume -c src/nextflow.config --with-docker 'src/pipe/mapping/'
+docker build src/pipe/mapping -t 'quality_control:0.0.1'
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/*_R{1,2}.fastq.gz' --trimmer 'UrQt' --paired true -resume -c src/nextflow.config -with-docker quality_control:0.0.1
