fba401bad2c085f97c0336e79b2f48aa49d373db
lmodolo <laurent.modolo@ens-lyon.fr>
2017-06-19 17:06:37 +0200

    quality_control_test.sh: new tests with tiny_dataset

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'UrQt' --paired false -resume && \
-# bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'cutadapt' --paired false -resume && \
-# bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'UrQt' --paired true
-docker build src/pipe/quality_control -t 'quality_control:0.0.1'
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' --paired true -c src/nextflow.config -with-docker quality_control:0.0.1
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' -c src/nextflow.config
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' -c src/nextflow.config
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' -c src/nextflow.config
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt' -c src/nextflow.config
