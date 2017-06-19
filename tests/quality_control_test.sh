801e7b88c98d0725f0697ee6c881301e6d77ca93
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-06-19 12:53:46 +0200

    quality_control_test.sh: test docker with paired-end

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -3,4 +3,4 @@
 # bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'cutadapt' --paired false -resume && \
 # bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'UrQt' --paired true
 docker build src/pipe/quality_control -t 'quality_control:0.0.1'
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'UrQt' --paired true -c src/nextflow.config -with-docker quality_control:0.0.1
+bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' --paired true -c src/nextflow.config -with-docker quality_control:0.0.1
