95bfa561a3b71657517b8eefa911cc63b5c739fb
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-06-26 17:44:14 +0200

    quality_control_docker_test.sh: compatible with profile based config

--- tests/quality_control_docker_test.sh
+++ tests/quality_control_docker_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp src/func/file_handle.py src/pipe/quality_control/ && \
 docker build src/pipe/quality_control -t 'quality_control:0.0.1' && \
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' -c src/nextflow.config -with-docker quality_control:0.0.1 && \
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' -c src/nextflow.config -with-docker quality_control:0.0.1 && \
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' -c src/nextflow.config -with-docker quality_control:0.0.1 && \
-bin/nextflow src/pipe/quality_control.nf --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt' -c src/nextflow.config -with-docker quality_control:0.0.1
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt'
