6e901a0908c53072f52571aeab1c6b07723e8480
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-07-20 14:41:17 +0200

    quality_control_test.sh: set number of cpu

--- tests/quality_control_test.sh
+++ tests/quality_control_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' && \
-bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' && \
-bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' && \
-bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt'
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' --cpu 2 && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' --cpu 2 && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' --cpu 2 && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt' --cpu 2 
