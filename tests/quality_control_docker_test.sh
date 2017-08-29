003e93792b9c1f1cf00525783e2cd353edd17a57
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-08-29 13:45:21 +0200

    quality_control_docker_test.sh: add test for gz fastq files

--- tests/quality_control_docker_test.sh
+++ tests/quality_control_docker_test.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 cp src/file_handle/src/file_handle.py src/pipe/quality_control/ && \
 docker build src/pipe/quality_control -t 'quality_control:0.0.1' && \
+
 bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'cutadapt' --cpu 2 && \
 bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'cutadapt' --cpu 2  && \
 bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq' --trimmer 'UrQt' --cpu 2  && \
-bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt' --cpu 2 
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq' --trimmer 'UrQt' --cpu 2
+
+ls -l data/examples/tiny_dataset/fastq/*.fastq | awk '{system("gzip "$9" -c > "$9".gz")}'
+
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq.gz' --trimmer 'cutadapt' --cpu 2 && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'cutadapt' --cpu 2  && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq.gz' --trimmer 'UrQt' --cpu 2  && \
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_docker --fastq_files 'data/examples/tiny_dataset/fastq/*tiny_S.fastq.gz' --trimmer 'UrQt' --cpu 2
