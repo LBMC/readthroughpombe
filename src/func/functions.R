384ef20dcf44eac3c877dfb385e0b5a638abf271
cburny <claire.burny@ens-lyon.fr>
2017-09-13 17:23:29 +0200

    functions.R: functions used in DESeq2 analysis

---
+++ src/func/functions.R
@@ -0,0 +1,79 @@
+#######################
+##### VolcanoPlot #####
+#######################
+VolcanoPlot <- function(res_deseq, pval.thresh, where, save, title) {
+  pdf(paste(where, save, sep = ""))
+  plot(res_deseq$log2FoldChange, -log10(res_deseq$padj), xlab = "shrinked log2FC", ylab = "-log10(pval)", main = title) 
+  nbup <- length(which(res_deseq$padj<=pval.thresh & res_deseq$log2FoldChange>0))
+  nbdown <- length(which(res_deseq$padj<=pval.thresh & res_deseq$log2FoldChange<0))
+  
+  abline(v = 0, col = "gray", lty = 2)
+  abline(h = -log10(pval.thresh), col = "red", lty = 2)     
+  legend("topright", legend = paste("pval threshold=", pval.thresh, sep = ""), col = "red", lty = 2, cex =0.75, bty = "n")
+  mtext(paste("nb up=", nbup, " - nb down=", nbdown, " on ", dim(res_deseq)[1], " genes", sep = ""), 3)
+  dev.off()
+}
+
+####################
+##### PvalHist #####
+####################
+PvalHist <- function(res_deseq, pval.thresh,where, save, title) {
+  pdf(paste(where, save, sep = ""))
+  hist(res_deseq$padj, breaks = 50, xlab = "pval", main = title )  
+  nbupdown <- length(which(res_deseq$padj<=pval.thresh))
+  mtext(paste("nb diffp=", nbupdown, " on ", dim(res_deseq)[1], " genes", sep = ""), 3)
+  abline(v = pval.thresh, col = "red", lty = 2)     
+  legend("topright", legend = paste("pval threshold=", pval.thresh, sep = ""), col = "red", lty = 2, cex =0.75, bty = "n")
+  dev.off()
+}
+
+############################
+##### MAplotBetween2Cn #####
+############################
+MAplotBetween2Cn <- function(res_deseq, where, save, title) {
+  pdf(paste(where, save, sep = ""))
+  plotMA(res_deseq, ylim=c(-2,2), main = title)
+  dev.off()
+}
+
+########################
+##### Heatmap100DE #####
+########################
+Heatmap100DE <- function(res_deseq, rld_count, pval.thresh, where, save, title) {
+  ind.ord.pval <- order(res_deseq$padj)
+  ind.signif <- which(res_deseq$padj[ind.ord.pval] <pval.thresh & is.na(res_deseq$padj[ind.ord.pval]) == F)
+  de <- ind.ord.pval[1:min(c(100, length(ind.signif)))]
+  de.to.clust <-t(assay(rld_count)[de, ])
+  pdf(paste(where, save, sep = ""))
+  heatmap(de.to.clust, xlab = "Genes", ylab = "Samples", margin = c(8, 8), cexRow = 0.9, cex.main = 0.9, main = title)
+  dev.off()
+}
+
+##############################
+##### DistBetweenSamples #####
+##############################
+DistBetweenSamples <- function(rld_count, where, save) {
+  save.pref <- strsplit(save, ".", fixed = T)[[1]][1]
+  sampleDists <- dist(t(assay(rld_count)))
+  sampleDistMatrix <- as.matrix(sampleDists)/max(sampleDists)
+  rownames(sampleDistMatrix) <- rld_count$condition
+  colnames(sampleDistMatrix) <- NULL
+  #colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(20)
+  p <- pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists)
+  pdf(paste(where, save.pref, "1.pdf", sep = ""))
+  print(p)
+  dev.off()
+  
+  pdf(paste(where, save.pref, "2.pdf", sep = ""))
+  plotPCA(rld_count, intgroup="condition")
+  dev.off()
+}
+
+########################
+##### PlotCountDen #####
+########################
+PlotCountDen <- function(count_df, where, save) {
+  pdf(paste(where, save, sep = ""))
+  ggplot(count_df, aes(log2(value+1), fill = samples, linetype=replicate)) + geom_density(alpha = 0.5) + xlab("") + ylab(expression("density"(log[10](count + 1))))
+  dev.off()  
+}
