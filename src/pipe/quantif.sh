05773d5c22313176ecb159d6e10bab015fca9be5
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-19 09:27:46 +0200

    quantif.sh: modify path to work within project dir:

--- src/pipe/quantif.sh
+++ src/pipe/quantif.sh
@@ -5,4 +5,4 @@
 
 module load nextflow/0.25.1
 
-nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping_sge --fastq_files '/scratch/cburny/Input_RNASeq/*.fastq.gz' --mapper 'bowtie2' --paired false --reference '/scratch/cburny/Input_RNASeq/Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta' --annotation '/scratch/cburny/Input_RNASeq/schizosaccharomyces_pombe.chr.gff' --quantifier 'rsem' --cpu 16
+nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping_sge --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --annotation 'data/2017_09_18_schizosaccharomyces_pombe.chr.gff' --quantifier 'rsem' --cpu 12
