2b3e7f930d9cf38f754b6825af38008cb8c7896b
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-20 11:44:17 +0200

    mapping.config: add rsem quantif option

--- src/pipe/quantif.sh
+++ src/pipe/quantif.sh
@@ -7,4 +7,4 @@ module load nextflow/0.25.1
 
 nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --quantif false --cpu 12 --bowtie2_parameters "--very-sensitive"
 
-nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --annotation 'data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --quantifier 'rsem' --cpu 12 --rsem_parameters_quantif "--fragment-length-mean 363.4 --fragment-length-sd 85.53354 --output-genome-bam"
+nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --annotation 'data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --quantifier 'rsem' --cpu 12
