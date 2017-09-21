2a69207048c71f1695264d950abc7ceea708e9ab
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-21 10:12:10 +0200

    quantif.sh: mage quantif.sh work locally

--- src/pipe/quantif.sh
+++ src/pipe/quantif.sh
@@ -5,6 +5,7 @@
 
 module load nextflow/0.25.1
 
-nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --quantif false --cpu 12 --bowtie2_parameters "--very-sensitive"
 
-nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference 'data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --annotation 'data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --quantifier 'rsem' --cpu 12
+nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference '../../data/readthroughpombe/data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --annotation '../../data/readthroughpombe/data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --quantifier 'rsem' --cpu 10 -w /home/laurent/data/readthroughpombe/work/
+
+nextflow src/pipe/mapping.nf -c src/nextflow.config -profile mapping --fastq_files 'results/quality_control/trimming/*.fastq.gz' --mapper 'bowtie2' --paired false --reference '../../data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --quantif false --cpu 10 -w /home/laurent/data/readthroughpombe/work/
