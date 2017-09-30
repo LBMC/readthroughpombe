813be8f18409bc1fad051880b7a5ebf34d3c3148
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-30 13:00:49 +0200

    pipeline.nf: add modularity object to build a todo list

---
+++ src/func/quality_control/adaptor_removal.sh
@@ -0,0 +1,12 @@
+${path.params.process_header}
+
+find . -type f -name "d*" | \
+sed 's/^.\\/d//g' | \
+awk '{system("mv d"\$0" "\$0)}'
+
+${path.params.cutadapt_module}
+${path.cmd_adaptor_removal(task.cpu, file)}
+${path.params.python2_unload_mnodule}
+
+${path.params.file_handle_module}
+${path.cmd_date('*.{zip,html}')}
