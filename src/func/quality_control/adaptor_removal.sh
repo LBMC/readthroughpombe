036af8acb06b254fa110b63a1fbf55adff93f0fa
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-01 19:51:42 +0200

    pipeline: fix adaptor removal process

--- src/func/quality_control/adaptor_removal.sh
+++ src/func/quality_control/adaptor_removal.sh
@@ -6,7 +6,7 @@ awk '{system("mv d"\$0" "\$0)}'
 
 ${path.params.cutadapt_module}
 ${path.cmd_adaptor_removal(task.cpu, file)}
-${path.params.python2_unload_mnodule}
+${path.params.python2_unload_module}
 
 ${path.params.file_handle_module}
 ${path.cmd_date('*.{zip,html}')}
