2f9f5de2a412ea49f8501765a6d89f152f9f47be
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-01 20:17:36 +0200

    pipeline_docker_test.sh: add -with-docker option

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -2,4 +2,4 @@
 
 docker build src/func/mapping -t 'mapping:0.0.1'&& \
 docker build src/func/mapping -t 'quality_control:0.0.1' && \
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt" -with-docker quality_control:0.0.1
