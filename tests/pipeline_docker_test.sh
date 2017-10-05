825484b7e5d7779cdbfedad0be50ef52cbb3ebc4
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-05 15:57:34 +0200

    pipeline_docker_test.sh: use right container

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker build src/func/mapping -t 'mapping:0.0.1'&& \
-docker build src/func/mapping -t 'quality_control:0.0.1' && \
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt" -with-docker quality_control:0.0.1
+docker build src/func/docker -t 'pipeline:0.0.1' && \
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt" -with-docker pipeline:0.0.1
