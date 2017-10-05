8f1b0011247988523e9b9874958094770d1e0b83
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-05 17:45:57 +0200

    pipeline: fix trimming problem for single-end

--- src/func/quality_control/cutadapt.sh
+++ src/func/quality_control/cutadapt.sh
@@ -3,8 +3,8 @@ ${path.params.process_header}
 env
 
 ${path.cmd_unsalt_file(file)}
-${path.trimming_module()}
-${path.cmd_trimming(task.cpu, file)}
+${path.params.cutadapt_module}
+${path.cmd_cutadapt(task.cpus, file)}
 ${path.params.python2_unload_module}
 
 ${path.params.file_handle_module}
