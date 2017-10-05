53987dfdabf3f0e6d53b4ac03352d89b1803b04e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-05 16:47:46 +0200

    pipeline: fix output for adaptor_removal

--- src/func/quality_control/adaptor_removal.sh
+++ src/func/quality_control/adaptor_removal.sh
@@ -4,8 +4,10 @@ env
 
 ${path.cmd_unsalt_file(file)}
 ${path.params.cutadapt_module}
-${path.cmd_adaptor_removal(task.cpu, file)}
+${path.cmd_adaptor_removal(task.cpus, file)}
 ${path.params.python2_unload_module}
 
+ls -l
 ${path.params.file_handle_module}
-${path.cmd_date('*.{zip,html}')}
+${path.cmd_date('*_cut*.fastq.gz')}
+${path.cmd_date('*_report.txt')}
