b03232df30842902e003668f55bf27329ee284cc
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-05 17:46:14 +0200

    pipeline_docker_test.sh: add paired-end test

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -2,3 +2,4 @@
 
 docker build src/func/docker -t 'pipeline:0.0.1' && \
 bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt" -with-docker pipeline:0.0.1
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --todo "fastqc+cutadapt+urqt" -with-docker pipeline:0.0.1
