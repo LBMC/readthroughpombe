e711354f350a7d75841deef16c4936b172a58d8e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-09 17:44:27 +0200

    pipeline.nf: add load reference (fasta)

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build src/func/docker -t 'pipeline:0.0.1' && \
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqtfastqc+multiqc"
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --todo "fastqc+cutadapt+urqt+fastqc+multiqc"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --todo "fastqc+cutadapt+urqt+fastqc+multiqc"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --todo "fastqc+cutadapt+urqt+fastqc+multiqc"
