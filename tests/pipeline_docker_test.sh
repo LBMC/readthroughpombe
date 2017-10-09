c9af880129302559b891f2e253671a8f4d4607bd
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-09 12:45:10 +0200

    pipeline_docker_test.sh: remove unecessary -with-docker option

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build src/func/docker -t 'pipeline:0.0.1' && \
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt" -with-docker pipeline:0.0.1
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --todo "fastqc+cutadapt+urqt" -with-docker pipeline:0.0.1
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --todo "fastqc+cutadapt+urqt"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --todo "fastqc+cutadapt+urqt"
