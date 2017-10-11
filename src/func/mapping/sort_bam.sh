d25836d4f8e1960f8829b1b38f29349e6b6778cc
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-11 16:04:20 +0200

    pipeline: fix mapping and quantif to not consume index and annot channel

--- src/func/mapping/sort_bam.sh
+++ src/func/mapping/sort_bam.sh
@@ -5,8 +5,8 @@ ${path.cmd_unsalt_file(bam)}
 ls -l
 
 ${path.params.samtools_module}
-${path.params.samtools} sort -@ ${task.cpus} -O BAM -o ${tagname}_sorted.bam ${path.unsalt_file_name(bam)}
+${path.params.samtools} sort -@ ${task.cpus} -O BAM -o ${tagname}_sort.bam ${path.unsalt_file_name(bam)}
 
 ${path.params.file_handle_module}
-${path.cmd_date('*_sorted.bam')}
+${path.cmd_date('*_sort.bam')}
 ls -l
