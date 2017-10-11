7281966bc94a04691b7c23a7ba01cbf81eadfb3f
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-11 09:44:40 +0200

    get_file_name.sh: make get_file_name step work for .gz files

--- src/func/get_file_name.sh
+++ src/func/get_file_name.sh
@@ -7,8 +7,9 @@ ${path.cmd_gz(task.cpus, file)}
 
 ${path.params.file_handle_module}
 ${path.cmd_date("*.gz")}
+ls -l
 
-find . -type f -name "*.gz" | \
-sed 's/^.\\///g' | \
+find . -name "*.gz" | \
+sed 's/^.*\\///g' | \
 awk '{system("mv "\$0" d"\$0)}'
 ls -l
