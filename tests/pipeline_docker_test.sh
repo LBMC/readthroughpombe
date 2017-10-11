d34069861bac2484f18806623994eea804288eac
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-11 09:35:10 +0200

    pipeline_docker_test.sh: add split_ref step

--- tests/pipeline_docker_test.sh
+++ tests/pipeline_docker_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build src/func/docker -t 'pipeline:0.0.1' && \
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --annot "data/examples/tiny_dataset/annot/tiny.gff" --todo "fastqc+cutadapt+urqt+fastqc+multiqc"
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --annot "data/examples/tiny_dataset/annot/tiny.gff" --todo "fastqc+cutadapt+urqt+fastqc+multiqc"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --annot "data/examples/tiny_dataset/annot/tiny.gff" --todo "fastqc+cutadapt+urqt+fastqc+multiqc+split_ref"
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/examples/tiny_dataset/fastq/*tiny_R{1,2}.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --annot "data/examples/tiny_dataset/annot/tiny.gff" --todo "fastqc+cutadapt+urqt+fastqc+multiqc+split_ref"
