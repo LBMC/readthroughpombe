adbed10585692b53e5984f18d4b24c761c806ff7
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-13 10:11:55 +0200

    pipeline_sge_test.sh: add modifications to run on the E5_test queue

--- tests/pipeline_sge_test.sh
+++ tests/pipeline_sge_test.sh
@@ -5,6 +5,11 @@ cp data/examples/tiny_dataset/fastq/tiny_R2.fastq data/examples/tiny_dataset/fas
 cp data/examples/tiny_dataset/fastq/tiny_R1.fastq data/examples/tiny_dataset/fastq/c2tiny_R1.fastq && \
 cp data/examples/tiny_dataset/fastq/tiny_R2.fastq data/examples/tiny_dataset/fastq/c2tiny_R2.fastq && \
 perl -pi -e 's/queue.*/queue = "E5_test"/g' src/pipe/conf/pipeline_sge.config
+perl -pi -e 's/cpus.*/cpus = 1/g' src/pipe/conf/pipeline_sge.config
+perl -pi -e 's/time.*//g' src/pipe/conf/pipeline_sge.config
+perl -pi -e 's/penv.*//g' src/pipe/conf/pipeline_sge.config
+
+
 module load nextflow/0.25.1
 
 nextflow src/pipeline.nf -c src/pipeline.config -profile sge --fastq "data/examples/tiny_dataset/fastq/*tiny_S.fastq" --fasta "data/examples/tiny_dataset/fasta/tiny_v2.fasta" --annot "data/examples/tiny_dataset/annot/tiny.gff" --todo "fastqc+cutadapt+cutadapt+fastqc+multiqc+split_ref+kallisto"
