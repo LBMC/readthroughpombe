4bf252bf1cfe33979508caef4644cf7ad92c80df
cburny <claire.burny@ens-lyon.fr>
2017-12-18 15:11:52 +0100

    prepare_readthrough_detection.sh: sort and convert annotation bed, index BAM and create 2 separated BAM files per BAM (one for forward reads and one for reverse reads

---
+++ src/prepare_readthrough_detection.sh
@@ -0,0 +1,60 @@
+##### Sort annotation
+sort -k1,1 -k4,4n data/ReferenceGenomes/schizosaccharomyces_pombe.chr.gff3 > data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr.gff3 &&\
+
+##### Convert gff annotation to bed 
+bash src/convert_gff3_to_bed.sh data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr.gff3 data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr.bed &&\
+
+##### Extract + and - features in 1 based coordinates
+Rscript src/format_bed.R &&\
+
+##### Index sorted BAM
+for filename in results/mapping/mapped/cut14-208/*.bam; do
+    samtools index "$filename" 
+done
+
+for filename in results/mapping/mapped/wt/*.bam; do
+    samtools index "$filename" 
+done
+
+for filename in results/mapping/mapped/cdc15-118/*.bam; do
+    samtools index "$filename" 
+done
+
+for filename in results/mapping/mapped/rrp6D/*.bam; do
+    samtools index "$filename" 
+done
+
+for filename in results/mapping/mapped/cut14-208_cdc15-118/*.bam; do
+    samtools index "$filename" 
+done
+
+##### Consider sense and antisense reads
+for filename in results/mapping/mapped/cut14-208/*sort.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
+    samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
+done
+
+for filename in results/mapping/mapped/wt/*sort.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
+    samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
+done
+
+for filename in results/mapping/mapped/cdc15-118/*sort.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
+    samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
+done
+
+for filename in results/mapping/mapped/rrp6D/*sort.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
+    samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
+done
+
+for filename in results/mapping/mapped/cut14-208_cdc15-118/*sort.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
+    samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
+done
