2d5618df3384906eda4c926a5ff2dc2d99535186
cburny <claire.burny@ens-lyon.fr>
2017-12-18 15:29:06 +0100

    prepare_readthrough_detection.sh, preprocess_MUSIC.sh, peak_calling_MUSIC.sh: Closes #13

--- src/peak_calling_MUSIC.sh
+++ src/peak_calling_MUSIC.sh
@@ -21,7 +21,9 @@ module load use.own
 module load MUSIC/6613c53
 module load SAMtools/1.5
 
-# Obtain window size
+# You need to tun  this script in the directory where you want to store your peak calling outputs per analysis 
+
+# Obtain window size per analysis
 MUSIC -get_per_win_p_vals_vs_FC -chip /scratch/cburny/Output_Music/output_cut14_forward/dedup -control /scratch/cburny/Output_Music/output_wt_forward/dedup -l_win_step 50 -l_win_min 200 -l_win_max 16000 &&\
 
 MUSIC -get_TF_peaks -chip /scratch/cburny/Output_Music/output_cut14_forward/dedup -control /scratch/cburny/Output_Music/output_wt_forward/dedup -mapp /scratch/cburny/Input_Music/temp -begin_l 100 -end_l 2000 -step 1.2 -l_mapp 50 -l_frag 363 -q_val 1 -l_p 0
