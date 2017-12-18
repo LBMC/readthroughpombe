77faeaec0267acb5e407a59ec182ab726d817e70
cburny <claire.burny@ens-lyon.fr>
2017-12-18 15:13:09 +0100

    prepare_readthrough_detection.sh: remove from oriented BAMs reads falling in annotation bed

--- src/prepare_readthrough_detection.sh
+++ src/prepare_readthrough_detection.sh
@@ -58,3 +58,64 @@ for filename in results/mapping/mapped/cut14-208_cdc15-118/*sort.bam; do
     samtools view -hb -F 0x10 "$filename" > "$res""_forward.bam" &&\
     samtools view -hb -f 0x10 "$filename" > "$res""_reverse.bam" 
 done
+
+##### Filter for BAM selected regions outside annotation
+for filename in results/mapping/mapped/cut14-208/*sort_forward.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.neg.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/wt/*sort_forward.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.neg.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/cdc15-118/*sort_forward.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.neg.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/rrp6D/*sort_forward.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.neg.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/cut14-208_cdc15-118/*sort_forward.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.neg.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/cut14-208/*sort_reverse.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.pos.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/wt/*sort_reverse.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.pos.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/cdc15-118/*sort_reverse.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.pos.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/rrp6D/*sort_reverse.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.pos.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
+
+for filename in results/mapping/mapped/cut14-208_cdc15-118/*sort_reverse.bam; do
+    res="$(echo $filename | cut -d'.' -f1)" &&\
+    samtools view -hb "$filename" -L data/ReferenceGenomes/2017_10_25_sorted_schizosaccharomyces_pombe.chr1.based.pos.bed -U "$res""_without.bam" > /dev/null
+    samtools index "$res""_without.bam"
+done 
