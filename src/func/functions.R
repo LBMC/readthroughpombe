0c1edada30862b637bd286252126be44d24677cf
claire.burny <claire.burny@ens-lyon.fr>
2018-01-09 18:17:54 +0100

    functions.R: change CreateRTName function to name transcript readthrough and add multiplot function to plot several ggplot2 plots on the same panel

--- src/func/functions.R
+++ src/func/functions.R
@@ -118,11 +118,12 @@ ComputeWindowsOverChrom <- function(vect.pos, size.chr) {
   return(tab.all)
 }
 
+
 ##### CreateRTName #####
 CreateRTName <- function(original_name) {
   split <- strsplit(original_name, ";")[[1]]
   parent <- na.omit(sapply(split, function(x) strsplit(x, "ID=")[[1]][2]))
-  name <- na.omit(sapply(split, function(x) strsplit(strsplit(x, "ID=")[[1]][2], 
+  name <- na.omit(sapply(split, function(x) strsplit(strsplit(x, "Parent=")[[1]][2], 
     ":")[[1]][2]))
   noun <- na.omit(sapply(split, function(x) strsplit(x, "Name=")[[1]][2]))
   return(paste(paste("ID=transcript:", name, ".rt", sep = ""), ";", paste("Name=", 
@@ -141,5 +142,52 @@ BarplotCountPerChrom <- function(data, col.by.type, title) {
     geom_text(aes(label = count), vjust = 1.6, color = "black", position = position_dodge(0.9), 
       size = 3.5) + scale_fill_brewer(palette = "Paired") + ggtitle(title)
   return(g)
+}
+
+# Multiple plot function: put several ggplot graphs saved in an object in the same 
+# layout: from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
 }
 
