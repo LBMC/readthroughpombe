ddf45606c06956fe326b0008378954ef5962c5c9
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-15 15:29:08 +0100

    update quantif.sh with last quantif cmd

--- src/pipe/quantif.sh
+++ src/pipe/quantif.sh
@@ -16,7 +16,8 @@ bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "res
 bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "results/quality_control/trimming/2017_09_26*.fastq.gz" --fasta '../../data/readthroughpombe/data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --todo "fastq+cutadapt+urqt+fastqc+multiqc+bowtie2" -w /home/laurent/data/readthroughpombe/work/
 
 
-bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "results/quality_control/trimming/*.fastq.gz" --index "results/mapping/index/2017_09_27_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.index*" --annot '../../data/readthroughpombe/data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --todo "bowtie2+rsem" -w /home/laurent/data/readthroughpombe/work/ -resume
+module load nextflow/0.25.1
+nextflow src/pipeline.nf -c src/pipeline.config -profile sge --fastq "results/quality_control/trimming/*.fastq.gz" --fasta "data/2017_10_26_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz" --annot '../../data/readthroughpombe/data/2017_09_19_schizosaccharomyces_pombe.chr.gtf' --todo "bowtie2+rsem" -w /scratch/lmodolo/readthroughpombe/work/ -resume
 
 
 module load nextflow/0.25.1
