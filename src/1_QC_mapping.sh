f299d244597240a2abda91f1163324be84907f3f
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-17 11:45:57 +0100

    1_QC_mapping.sh: clear steps for the QC & mapping analysis

---
+++ src/1_QC_mapping.sh
@@ -0,0 +1,9 @@
+# script to run the pipeline.nf on the RNASeq data
+
+# with docker
+
+# QC analysis
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "data/RNASeq/trimming/2017_04_14*.fastq.gz" --todo "fastq+cutadapt+urqt+fastqc+multiqc"
+
+# mapping
+bin/nextflow src/pipeline.nf -c src/pipeline.config -profile docker --fastq "results/quality_control/trimming/2017_09_26*.fastq.gz" --fasta 'data/readthroughpombe/data/2017_09_18_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta.gz' --todo "bowtie2"
