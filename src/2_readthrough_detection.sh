70f7bc0756e1813c73c7813e483719039a6c2c29
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-26 16:25:01 +0100

    2_readthrough_detection.sh: analysis of the two mutant for RT detection

--- src/2_readthrough_detection.sh
+++ src/2_readthrough_detection.sh
@@ -1,3 +1,5 @@
 # script to run the pipeline readthrough.nf on the data
 
-bin/nextflow src/readthrough.nf -c src/pipe/conf/readthrough_docker.config -profile docker --bams "results/mapping/mapping/2018_04_14*.bam" --annotation "data/2017_09_19_schizosaccharomyces_pombe.chr.gff3" -resume
+bin/nextflow src/readthrough.nf -c src/pipe/conf/readthrough_docker.config --name "rrp6D" --bams "data/bams_rrp6D.csv" --annotation "data/2017_09_19_schizosaccharomyces_pombe.chr.gff3" --index "results/mapping/indexing/2018_01_19_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.index*" --genome "data/Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fa" --reads_size 50 --frag_size 363
+
+bin/nextflow src/readthrough.nf -c src/pipe/conf/readthrough_docker.config --name "cut14" --bams "data/bams_cut14.csv" --annotation "data/2017_09_19_schizosaccharomyces_pombe.chr.gff3" --index "results/mapping/indexing/2018_01_19_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.index*" --genome "data/Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fa" --reads_size 50 --frag_size 363 -resume
