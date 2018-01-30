LBMC/readthroughpombe

commit 662f9c9bec95cb1ee349e5ace4f46b98fbfabf9a

readthrough.nf: substract annotation from peaks to fix #21

src/readthrough.nf
@@ -603,7 +603,8 @@ process peak_merge_forward {
     file "*RT_forward.bed" into rt_forward
   script:
   """
-  cat ${peaks} ${annotation} > to_merge_forward.bed
+  bedtools substract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
+  cat peaks_cleaned.bed ${annotation} > to_merge_forward.bed
   bedtools sort -i to_merge_forward.bed > to_merge_forward_s.bed
 
   tac to_merge_forward_s.bed | \
@@ -640,7 +641,8 @@ process peak_merge_reverse {
     file "*RT_reverse.bed" into rt_reverse
   script:
   """
-  cat ${peaks} ${annotation} > to_merge_reverse.bed
+  bedtools substract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
+  cat peaks_cleaned.bed ${annotation} > to_merge_reverse.bed
   bedtools sort -i to_merge_reverse.bed > to_merge_reverse_s.bed
 
   awk -v OFS='\t' '{

commit ce26ff76f4d1ecfdc34e08e2c9646379914a4214

readthrough.nf: fix bug introduced in 662f9c9b

src/readthrough.nf
@@ -603,7 +603,7 @@ process peak_merge_forward {
     file "*RT_forward.bed" into rt_forward
   script:
   """
-  bedtools substract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
+  bedtools subtract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
   cat peaks_cleaned.bed ${annotation} > to_merge_forward.bed
   bedtools sort -i to_merge_forward.bed > to_merge_forward_s.bed
 
@@ -641,7 +641,7 @@ process peak_merge_reverse {
     file "*RT_reverse.bed" into rt_reverse
   script:
   """
-  bedtools substract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
+  bedtools subtract -a ${peaks} -b ${annotation} > peaks_cleaned.bed
   cat peaks_cleaned.bed ${annotation} > to_merge_reverse.bed
   bedtools sort -i to_merge_reverse.bed > to_merge_reverse_s.bed
 