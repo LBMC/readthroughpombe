fb3e6f6e5497cc67dfd3860f1467507ee678d442
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-05 17:57:46 +0100

    33_readthrough_quantification.sh: add mean and sd fragment size

--- src/3_readthrough_quantification.sh
+++ src/3_readthrough_quantification.sh
@@ -4,4 +4,7 @@ bin/nextflow src/readthrough_quantif.nf -c src/pipe/conf/readthrough_docker.conf
   --annotation_forward "results/readthrough/peak_calling/2018_01_31_*_RT_forward.bed" \
   --annotation_reverse "results/readthrough/peak_calling/2018_01_31_*_RT_reverse.bed" \
   --fasta 'data/2017_09_19_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta' \
-  --bam "results/mapping/mapping/2018_01_19_*_rev_sort.bam" -resume -w /home/laurent/data/work/
+  --bam "results/mapping/mapping/2018_01_19_*_rev_sort.bam" \
+  --mean_size 363.4 \
+  --sd_size 85.53354 \
+  -resume -w /home/laurent/data/work/
