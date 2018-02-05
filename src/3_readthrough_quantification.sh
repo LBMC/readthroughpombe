7b68373613ce39c94a8acea572a886d241c03e2d
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-05 17:47:22 +0100

    3_readthrough_quantification.sh: cmd to run readthrough_quantif.nf

---
+++ src/3_readthrough_quantification.sh
@@ -0,0 +1,7 @@
+# script to quantifiy the readthrough events
+
+bin/nextflow src/readthrough_quantif.nf -c src/pipe/conf/readthrough_docker.config \
+  --annotation_forward "results/readthrough/peak_calling/2018_01_31_*_RT_forward.bed" \
+  --annotation_reverse "results/readthrough/peak_calling/2018_01_31_*_RT_reverse.bed" \
+  --fasta 'data/2017_09_19_Schizosaccharomyces_pombe.ASM294v2.30.dna.genome.fasta' \
+  --bam "results/mapping/mapping/2018_01_19_*_rev_sort.bam" -resume -w /home/laurent/data/work/
