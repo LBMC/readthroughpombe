80a68f4b2eecdf0c86381fcf05ef0e077b885d9b
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-14 16:53:53 +0100

    4_readthrough_DEA.sh: make bed from DE readthrough

--- src/4_readthrough_DEA.sh
+++ src/4_readthrough_DEA.sh
@@ -1,3 +1,11 @@
 # script to run DEA on the readthrough resutls
 
 Rscript src/DEA.R
+
+find results/readthrough/DEA/RT/ -name "*_RT.csv" | while read fname;
+do
+  echo "$fname"
+  awk -F "," 'NR >= 2 {if($7 <= 0.05) {print $1}}' $fname | sed 's/"//g' > $fname.DE.csv
+  grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_forward.bed > $fname.bed
+  grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_reverse.bed >> $fname.bed
+done
