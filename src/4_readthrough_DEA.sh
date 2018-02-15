ca7db771d6278d66aa1fc836b58652fedf675d5b
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-15 13:56:11 +0100

    4_readthrough_DEA.sh: generate bed for all DEA

--- src/4_readthrough_DEA.sh
+++ src/4_readthrough_DEA.sh
@@ -2,10 +2,18 @@
 
 Rscript src/DEA.R
 
-find results/readthrough/DEA/RT/ -name "*_RT.csv" | while read fname;
+find results/readthrough/DEA/RT/ -name "*.csv" | while read fname;
 do
   echo "$fname"
   awk -F "," 'NR >= 2 {if($7 <= 0.05) {print $1}}' $fname | sed 's/"//g' > $fname.DE.csv
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_forward.bed > $fname.bed
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_reverse.bed >> $fname.bed
+done
+
+find results/readthrough/DEA/T/ -name "*.csv" | while read fname;
+do
+  echo "$fname"
+  awk -F "," 'NR >= 2 {if($7 <= 0.05) {print $1}}' $fname | sed 's/"//g' > $fname.DE.csv
+  grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_transcript_annotation_forward.bed > $fname.bed
+  grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_transcript_annotation_reverse.bed >> $fname.bed
 done
