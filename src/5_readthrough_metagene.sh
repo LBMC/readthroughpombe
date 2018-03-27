ab35ef57ab3cc97d4d7ff5350de8c94f4fc01a8d
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-03-27 11:18:09 +0200

    5_readthrough_metagene.sh: use correct annotation

--- src/5_readthrough_metagene.sh
+++ src/5_readthrough_metagene.sh
@@ -1,8 +1,8 @@
 # script to plot metagene of the readthrough events
 
 bin/nextflow src/readthrough_metagene.nf -c src/pipe/conf/readthrough_docker.config \
-  --annotation_forward "results/readthrough/peak_calling/2018_02_28_*_peak_calling_forward.bed" \
-  --annotation_reverse "results/readthrough/peak_calling/2018_02_28_*_peak_calling_reverse.bed" \
+  --annotation_forward "results/readthrough/peak_calling/2018_02_28_*_RT_forward.bed" \
+  --annotation_reverse "results/readthrough/peak_calling/2018_02_28_*_RT_reverse.bed" \
   --bam "results/readthrough/bams/*/*.bam" \
   --mean_size 363 \
   -resume -w /home/laurent/data/work/ -with-dag results/readthrough/metagene_dag.pdf \
