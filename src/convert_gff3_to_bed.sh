863dc3d6567c83250f18f01872c34bcb5ddd2e4f
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-04-17 11:55:00 +0200

    code cleanup

--- src/convert_gff3_to_bed.sh
+++
@@ -1,5 +0,0 @@
-##### Convert gff3 to bed 
-# $1: input gff3
-# $2: output bed
-
-gff2bed < $1 > $2
--- src/date.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This script renames files using file_handle. 
-# bash date.sh "file"
-
-python3 ./src/file_handle/src/file_handle.py -f $1
