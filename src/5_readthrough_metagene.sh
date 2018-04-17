3943d524d072031bd7c34bf2ea6d444b30567472
Laurent Modolo <laurent@modolo.fr>
2018-04-17 11:38:55 +0200

    5_readthrough_metagene.sh: add sge analysis

--- src/5_readthrough_metagene.sh
+++ src/5_readthrough_metagene.sh
@@ -7,6 +7,7 @@ find ../../../mapping/bams/ -name "*.bam" | wc -l
 find ../../../mapping/bams/ -name "*.bam" | perl -pe "s/(.*(PLBD\d+)-(.+)-Galaxy.*)/\2 \3 \1/g" | sort -k1 > target.txt
 find ../../../mapping/mapping/ -name "*.bam" | perl -pe "s/.*(PLBD\d+)_(.*)_R\d.*/\1 \1_\2/g" | sort -k1 > link.txt
 join target.txt link.txt | awk '{system("ln -s "$3" "$4"_"$2".bam")}'
+ml file_handle
 file_handle.py -f *.bam
 rm *cdc15*
 cd ~/projects/readthroughpombe/
@@ -24,9 +25,11 @@ bin/nextflow src/readthrough_metagene.nf -c src/pipe/conf/readthrough_docker.con
   -resume -w /home/laurent/data/work/ -with-dag results/readthrough/metagene_dag.pdf \
   -with-timeline results/readthrough/metagene_timeline
 
-bin/nextflow src/readthrough_metagene.nf -c src/pipe/conf/readthrough_sge.config \
-  --annotation "results/readthrough/peak_calling/2018_02_28_*_RT_forward.bed" \
-  --bam "results/readthrough/bams/metagene/*.bam" \
-  -resume -w /home/laurent/data/work/ -with-dag results/readthrough/metagene_dag.pdf \
-  -with-timeline results/readthrough/metagene_timeline
+ml nextflow
+cd /scratch/lmodolo/readthroughpombe/
+nextflow src/readthrough_metagene.nf -c src/pipe/conf/readthrough_sge.config \
+  --annotation "/scratch/lmodolo/readthroughpombe/results/readthrough/peak_calling/2018_02_28_*_RT_forward.bed" \
+  --bam "/scratch/lmodolo/readthroughpombe/results/readthrough/bams/metagene/*.bam" \
+  -resume -w /scratch/lmodolo/work/ -with-dag /scratch/lmodolo/readthroughpombe/results/readthrough/metagene_dag.pdf \
+  -with-timeline /scratch/lmodolo/readthroughpombe/results/readthrough/metagene_timeline
 
