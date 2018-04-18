433e524d13551d764289d58d403463a352d5c086
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-04-18 15:20:43 +0200

    4_readthrough_DEA.sh: add bed for all genes not only DEG

--- src/4_readthrough_DEA.sh
+++ src/4_readthrough_DEA.sh
@@ -9,6 +9,9 @@ do
   awk -F "," 'NR >= 2 {if($7 <= 0.05) {print $1}}' $fname | sed 's/"//g' > $fname.DE.csv
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_forward.bed > $fname.bed
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_RT_annotation_reverse.bed >> $fname.bed
+  awk -F "," 'NR >= 2 {print $1}' $fname | sed 's/"//g' > $fname.all.csv
+  grep -F -f $fname.all.csv results/readthrough/transcript/2018_02_14_RT_annotation_forward.bed > $fname.all.bed
+  grep -F -f $fname.all.csv results/readthrough/transcript/2018_02_14_RT_annotation_reverse.bed >> $fname.all.bed
 done
 
 find results/readthrough/DEA/T/ -name "*.csv" | while read fname;
@@ -17,4 +20,7 @@ do
   awk -F "," 'NR >= 2 {if($7 <= 0.05) {print $1}}' $fname | sed 's/"//g' > $fname.DE.csv
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_transcript_annotation_forward.bed > $fname.bed
   grep -F -f $fname.DE.csv results/readthrough/transcript/2018_02_14_transcript_annotation_reverse.bed >> $fname.bed
+  awk -F "," 'NR >= 2 {print $1}' $fname | sed 's/"//g' > $fname.all.csv
+  grep -F -f $fname.all.csv results/readthrough/transcript/2018_02_14_transcript_annotation_forward.bed > $fname.all.bed
+  grep -F -f $fname.all.csv results/readthrough/transcript/2018_02_14_transcript_annotation_reverse.bed >> $fname.all.bed
 done
