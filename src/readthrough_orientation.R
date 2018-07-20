90d7743cebf33b7e971325b57671528aba448974
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-07-20 10:00:42 +0200

    readthrough_orientation.sh: add rrp6D analysis

--- src/readthrough_orientation.R
+++ src/readthrough_orientation.R
@@ -1,12 +1,56 @@
 library("tidyverse")
 
 # we want to look if the closest transcript to the RT are more in the same
-# strand or in the opposite strand
+# strand or in the opposite strand for cut14
 
 bed_colnames <- c("chrom", "chromStart", "chromEnd", "name", "score",
                   "strand", "specie", "type", "score_2", "infos")
 file_RT <- "results/readthrough/DEA/RT/wt_vs_cut14_208_RT_lfc_greater_than_0/2018_02_15_wt_vs_cut14_208_RT.csv.bed"
 file_T <- "results/readthrough/DEA/T/wt_vs_cut14_208_lfc_greaterAbs_than_0.5/2018_02_15_wt_vs_cut14_208.csv.DE.csv.all.bed"
+
+DE_bed <- read_tsv(file_RT, col_names = FALSE) %>%
+  setNames(bed_colnames) %>%
+  mutate(DE = TRUE,
+         closest_pos = NA,
+         closest_strand = NA)
+all_bed <- read_tsv(file_T, col_names = FALSE) %>%
+  setNames(bed_colnames) %>%
+  mutate(DE = FALSE,
+         chromMiddle = round(abs(chromStart - chromEnd) / 2) +
+         ifelse(strand %in% "+",chromStart, chromEnd))
+
+for (i in DE_bed %>% nrow() %>% seq_len()) {
+  chrom <- DE_bed %>% select(chrom) %>% slice(i)
+  pos <- DE_bed %>% select(chromEnd) %>% slice(i) %>% as.integer()
+  DE_bed$closest_pos[i] <- all_bed %>%
+          filter(chrom == chrom) %>%
+          pull(chromMiddle) %>%
+          -pos %>%
+          abs() %>%
+          which.min()
+  DE_bed$closest_strand[i] <- all_bed %>%
+    filter(chrom == chrom) %>%
+    slice(DE_bed$closest_pos[i]) %>%
+    pull(strand)
+}
+
+DE_bed %>% pull(strand) %>% as.factor() %>% table()
+DE_bed %>% pull(closest_strand) %>% as.factor() %>% table()
+table(DE_bed %>% pull(strand) %>% paste0("RT") %>% as.factor(),
+      DE_bed %>% pull(closest_strand) %>% paste0("closest") %>% as.factor()) %>%
+  print()
+table(DE_bed %>% pull(strand) %>% paste0("RT") %>% as.factor(),
+      DE_bed %>% pull(closest_strand) %>% paste0("closest") %>% as.factor()) %>%
+  as.matrix() %>%
+  chisq.test()
+
+# we want to look if the closest transcript to the RT are more in the same
+# strand or in the opposite strand for rrp6D
+
+bed_colnames <- c("chrom", "chromStart", "chromEnd", "name", "score",
+                  "strand", "specie", "type", "score_2", "infos")
+file_RT <- "results/readthrough/DEA/RT/wt_vs_rrp6D_RT_lfc_greater_than_0/2018_02_15_wt_vs_rrp6D_RT.csv.bed"
+file_T <- "results/readthrough/DEA/T/wt_vs_rrp6D_lfc_greaterAbs_than_0.5/2018_02_15_wt_vs_rrp6D.csv.DE.csv.all.bed"
 
 DE_bed <- read_tsv(file_RT, col_names = FALSE) %>%
   setNames(bed_colnames) %>%
